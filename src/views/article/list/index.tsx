91c12a0e43e2f9cb8e1cfb7f2ccc554d86d97fdc
zhangxu <zansderjs@gmail.com>
2020-06-01 17:23:03 +0800

    添加文章列表

---
+++ src/views/article/list/index.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ArticleList extends Component {
+  render() {
+    return (
+      <section className="article-list-container">文章列表</section>
+    )
+  }
+}
+
+export default ArticleList;
