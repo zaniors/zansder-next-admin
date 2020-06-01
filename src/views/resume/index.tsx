434be21dfe8ae8f20556775cc71e08d49d36ccb2
zhangxu <zansderjs@gmail.com>
2020-06-01 17:23:13 +0800

    添加简历页面

---
+++ src/views/resume/index.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Resume extends Component {
+  render() {
+    return (
+      <section className="resume container">简历</section>
+    )
+  }
+}
+
+export default Resume;
