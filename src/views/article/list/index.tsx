b339168b7737e51cb6e93dea922c442b6d7708d9
zhangxu <zansderjs@gmail.com>
2020-06-02 18:04:15 +0800

    文章列表接口

--- src/views/article/list/index.tsx
+++ src/views/article/list/index.tsx
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
+import { Switch, Card, Avatar } from 'antd';
+import { getArticles } from '../article.service';
+
+const { Meta } = Card;
 
 class ArticleList extends Component {
+  state = {
+    loading: true,
+  };
+
+  componentDidMount = () => {
+    this.loadArticles();
+  }
+
+  onChange = (checked: boolean) => {
+    this.setState({ loading: !checked });
+  };
+
+  loadArticles = async () => {
+    const data = await getArticles();
+    console.log(data)
+  }
+
   render() {
+    const { loading } = this.state;
+
     return (
-      <section className="article-list-container">文章列表</section>
+      <section className="article-list-container">
+        <Switch checked={!loading} onChange={this.onChange} />
+
+        <Card style={{ width: 300, marginTop: 16 }} loading={loading}>
+          <Meta
+            avatar={
+              <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+            }
+            title="Card title"
+            description="This is the description"
+          />
+        </Card>
+      </section>
     )
   }
 }
